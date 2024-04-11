afa7ab455f72a678a1d85c14c1c07e6d2e0c4564
Ubaid Asim <asimubaid6@gmail.com>
2024-04-12 04:47:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// =================== TYPING ANIMATION ========================
+
+var typed = new Typed (".typing" ,{
+    Strings:["","Web Designer","Web Devoloper","Graphic Designer","Data Management"],
+    typeSpeed:130,
+    BackSpeed:60,
+    loop:true
+})
